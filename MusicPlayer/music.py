b1cd2d8ded32c6981848f2b889a6b49be7d4c07c
Atharva Chandwadkar <chandwadkar28@gmail.com>
2021-05-05 20:55:18 +0530

    Update MusicPlayer

--- MusicPlayer/music.py
+++ MusicPlayer/music.py
@@ -4,12 +4,17 @@ from pygame import mixer
 
 class MusicPlayer:
     def __init__(self, window ):
-        window.geometry('320x100'); window.title('Music Player'); window.resizable(0,0)
-        Load = Button(window, text = 'Load',  width = 10, font = ('Times', 10), command = self.load)
-        Play = Button(window, text = 'Play',  width = 10,font = ('Times', 10), command = self.play)
-        Pause = Button(window,text = 'Pause',  width = 10, font = ('Times', 10), command = self.pause)
-        Stop = Button(window ,text = 'Stop',  width = 10, font = ('Times', 10), command = self.stop)
-        Load.place(x=0,y=20);Play.place(x=110,y=20);Pause.place(x=220,y=20);Stop.place(x=110,y=60)
+        window.geometry('320x100')
+        window.title('Music Player')
+        window.resizable(0,0)
+        load = Button(window, text = 'Load',  width = 10, font = ('Times', 10), command = self.load)
+        play = Button(window, text = 'Play',  width = 10,font = ('Times', 10), command = self.play)
+        pause = Button(window,text = 'Pause',  width = 10, font = ('Times', 10), command = self.pause)
+        stop = Button(window ,text = 'Stop',  width = 10, font = ('Times', 10), command = self.stop)
+        load.place(x=0,y=20)
+        play.place(x=110,y=20)
+        pause.place(x=220,y=20)
+        stop.place(x=110,y=60)
         self.music_file = False
         self.playing_state = False
     def load(self):
