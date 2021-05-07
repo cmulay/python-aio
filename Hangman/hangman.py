2620bde6246311d0fa299e8fe208177238e6c0d2
Atharva Chandwadkar <chandwadkar28@gmail.com>
2021-05-07 20:07:47 +0530

    Added Hangman

---
+++ Hangman/hangman.py
@@ -0,0 +1,48 @@
+import random
+from words import words
+import string
+
+
+def get_valid_word(words):
+    word = random.choice(words)
+    while '-' in word or ' ' in word:
+        word = random.choice(words)
+    return word.upper()
+
+
+def hangman():
+    word = get_valid_word(words)
+    word_letters = set(word)
+    alphabet = set(string.ascii_uppercase)
+    used_letters = set()
+
+    lives = 5
+
+    while len(word_letters) > 0 and lives > 0:
+        print(f'You have {lives} lives left and you have used these letters: ', ' '.join(used_letters))
+        word_list = [letter if letter in used_letters else '-' for letter in word]
+        print('Current word: ', ' '.join(word_list))
+        user_letter = input('Guess a letter: ').upper()
+
+        if user_letter in alphabet - used_letters:
+            used_letters.add(user_letter)
+            if user_letter in word_letters:
+                word_letters.remove(user_letter)
+                print('')
+
+            else:
+                lives = lives - 1
+                print('\nYour letter,', user_letter, 'is not in the word.')
+        elif user_letter in used_letters:
+            print('\nYou have already used that letter. Guess another letter.')
+        else:
+            print('\nThat is not a valid letter.')
+
+    if lives == 0:
+        print('You died, sorry. The word was', word)
+    else:
+        print('YAY! You guessed the word', word, '!!')
+
+
+if __name__ == '__main__':
+    hangman()
