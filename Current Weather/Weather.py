ff6ced1ae33904f42ef76fc6b2f1a674f8e2bd80
cmulay <codewithchin@gmail.com>
2021-05-08 13:30:53 +0530

    Fixed typos, removed unnecessary () and mentioned contributors

--- Current Weather/Weather.py
+++ Current Weather/Weather.py
@@ -1,7 +1,11 @@
+# Developed by @Davekibh
+# Improved by @kaar07
+# Optimised by @cmulay
+
 import requests
 
 
-def kelvinToC(temperature):
+def kelvin_to_c(temperature):
     return round(float(temperature - 273.15), 2)
 
 
@@ -20,25 +24,28 @@ response = requests.get(complete_url)
 x = response.json()
 
 
-if(x["cod"] == 401):
+if x["cod"] == 401:
     print("Invalid API keys, please check if your API key and ensure that it is activated.")
     print("In case you don't have an API key, sign up at https://openweathermap.org and check for API keys in accounts section.")
-elif(x["cod"] == 429):
+
+elif x["cod"] == 429:
     print("You exceeded the 60/min established limit of API calls. For more info https://openweathermap.org/faq#error401")
-elif(x["cod"] == "404"):
+
+elif x["cod"] == "404":
     print("Invalid API call. Check the city name. Check at http://bulk.openweathermap.org/sample/")
+
 else:
     y = x["main"]
     current_temperature = y["temp"]
     current_pressure = y["pressure"]
-    current_humidiy = y["humidity"]
-    celsius = kelvinToC(current_temperature)
+    current_humidity = y["humidity"]
+    celsius = kelvin_to_c(current_temperature)
     z = x["weather"]
     weather_description = z[0]["description"]
     print(f"Weather report for {city_name}.\nSource: https://openweathermap.org")
     print(f"""
     Temperature (in K and in °C):\t\t{current_temperature} Kelvin / {celsius} °C
-    Atmostpheric Pressure (in hPa unit):\t{current_pressure}
-    Humidity (in percentage):\t\t\t{current_humidiy}
+    Atmospheric Pressure (in hPa unit):\t{current_pressure}
+    Humidity (in percentage):\t\t\t{current_humidity}
     Description : {weather_description.capitalize()}
     """)
