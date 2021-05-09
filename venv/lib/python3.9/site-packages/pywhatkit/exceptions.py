5ba5c6f815a77dbf87cad7dd6df185d79bfb9eb9
cmulay <codewithchin@gmail.com>
2021-05-09 20:58:30 +0530

    added updated packages

---
+++ venv/lib/python3.9/site-packages/pywhatkit/exceptions.py
@@ -0,0 +1,22 @@
+class CountryCodeException(Exception):
+    pass
+
+
+class CallTimeException(Exception):
+    pass
+
+
+class InternetException(Exception):
+    pass
+
+
+class FilePathException(Exception):
+    pass
+
+
+class InvalidBrowserName(Exception):
+    pass
+
+
+class UnsupportedEmailProvider(Exception):
+    pass
